05140d74b3612c0783bb3ba2ac3d953b44a18b28
MotyaDev <137160953+MotyaDev@users.noreply.github.com>
2025-02-02 14:04:03 +0300

    Update artixde.sh

--- artixde.sh
+++ artixde.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+if ! [[ $(id -u) = 0 ]]; then
+    echo "This script must be run as root!"
+    exit 1
+fi
+
+export install_log="$(mktemp -t install_logXXX)"
+export red="\033[1;31m"
+export green="\033[1;32m"
+export cyan="\033[0;36m"
+export normal="\033[0m"
+
 # This updates your system
 pacman -Syyu
 
